8f378401de6c24445470deae75e77b3ff9d87055
Gachal29 <taisuke0129t@gmail.com>
2023-10-23 00:47:36 +0900

    コード整形

--- api/serializers.py
+++ api/serializers.py
@@ -12,26 +12,28 @@ class GachalSerializer(serializers.Serializer):
         # extraction_numとweights周り
         if data["same"]:
             # extraction_num
-            if not "extraction_num" in data:
+            if "extraction_num" not in data:
                 raise ValidationError("'same'がtrueの際は、'extraction_num'は必須です。")
             if data["extraction_num"] == 1:
                 data["same"] = False
-            
+
             # weights
-            if not "weights" in data:
+            if "weights" not in data:
                 data["weights"] = [1] * len(data["contents"])
             else:
                 if len(data["contents"]) in len(data["weights"]):
                     raise ValidationError("'content'と'weights'の個数が一致しません。")
         else:
             # extraction_num
-            if not "extraction_num" in data:
+            if "extraction_num" not in data:
                 data["extraction_num"] = len(data["contents"])
             else:
                 if data["extraction_num"] > len(data["contents"]):
-                    raise ValidationError("sameがfalseの際は、'extraction_num'は'contents'の要素数以下に設定してください。")
+                    raise ValidationError(
+                        "sameがfalseの際は、'extraction_num'は'contents'の要素数以下に設定してください。"
+                    )
         return data
-    
+
     def validate_contents(self, contents):
         # contentsの存在を確認
         if len(contents) == 0:
